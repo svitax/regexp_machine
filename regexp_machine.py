ceebcab7807c93301e6d6d0f408fc338b9b5552e
Igor Barale <svitaxiom@gmail.com>
2021-07-01 16:29:15 -0400

    Implement NFA class

--- regexp_machine.py
+++ regexp_machine.py
@@ -26,3 +26,17 @@ s3 = State(False)
 s1.addTransitionForSymbol('a', s2)
 s1.addTransitionForSymbol('a', s3)
 print(s1.getTransitionsForSymbol('a'))  # {a: [s2, s3]}
+
+
+# NFA State Class
+class NFA:
+    def __init__(self, inState, outState):
+        self.inState = inState
+        self.outState = outState
+
+    # Tests whether this NFA matches the string.
+    # Delegates to the input state
+    def test(self, string):
+        return self.inState.test(string)
+
+
