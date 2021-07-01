f3a520196aba9677294d1cd25c4bc4f830b88a2a
Igor Barale <svitaxiom@gmail.com>
2021-07-01 16:07:50 -0400

    Implemented addTransitionForSymbol and getTransitionForSymbol in State class

--- regexp_machine.py
+++ regexp_machine.py
@@ -1,19 +1,24 @@
 # FA State Class
 
+from collections import defaultdict
+
 
 class State:
     def __init__(self, accepting=False):
         self.accepting = accepting
-        self.transitionMap = {}
+        self.transitionMap = defaultdict(list)
 
     def addTransitionForSymbol(self, symbol, state):
-        # Implementation goes here
-        return None
+        self.transitionMap[symbol] += [state]
 
     def getTransitionsForSymbol(self, symbol):
-        # Implementation goes here
-        return None
+        return {symbol: self.transitionMap[symbol]}
 
 
 s1 = State(False)
 s2 = State(True)
+s3 = State(False)
+
+s1.addTransitionForSymbol('a', s2)
+s1.addTransitionForSymbol('a', s3)
+print(s1.getTransitionsForSymbol('a'))  # {a: [s2, s3]}
